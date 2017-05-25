563b30d087b55eddbbb05aa163aeac9e28fb1ebe
Erin Cole <Erin Cole>
2017-05-25 10:06:00 -0700

    initial skeleton of function with unit test

---
+++ showStats.py
@@ -0,0 +1,16 @@
+# Erin Cole
+# Filename: showStats
+# Description: This file shows initial statistical information about a database.#               It is meant to be run after establishing a connection to a SQL
+#               database
+
+
+
+# Function Name: showStats()
+# Parameters: cnxn, a connection to a database server
+# Descsroption: this function returns a formmated display of the initial 
+#               information typically queried from a database after a connection#               has been established.
+# Return Value: a formatted string of the initial statistics of a database
+
+def showStats(cnxn):
+         
+    
